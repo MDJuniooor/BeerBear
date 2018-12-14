1926237e683f994ace396c9c2fd59e62edaee877
stiria06 <32118818+stiria06@users.noreply.github.com>
2018-12-14 20:57:02 +0900

    Add files via upload

--- frontend/components/SearchHeader.js
+++ frontend/components/SearchHeader.js
@@ -5,12 +5,12 @@ import { Header, Item, Icon, Input } from "native-base";
 class SearchHeader extends Component {
   render() {
     return (
-        <Header style={{ height: 80, }} transparent searchBar rounded>
+      <Header style={{ height: 80, }} transparent searchBar rounded>
         <Item>
           <Icon name="ios-search" />
           <Input
-            placeholder="검색어를 입력해주세요"
-            onChangeText={this.props.onChangeText}
+            placeholder = "검색어를 입력해주세요"
+            onChangeText = {this.props.onChangeText}
             returnKeyType="search"
             onSubmitEditing={this.props.beerSearch}
           />
