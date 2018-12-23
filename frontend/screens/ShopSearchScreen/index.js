0cd8013758272bf9c6c9b9f3e88ed2185ffbb7a5
Zunwu <44821288+Zunwu@users.noreply.github.com>
2018-12-23 16:10:14 +0900

    Update index.js

--- frontend/screens/ShopSearchScreen/index.js
+++ frontend/screens/ShopSearchScreen/index.js
@@ -7,8 +7,8 @@ import {
 import ShopScreen from "./ShopScreen";
 import Map from "./Map";
 import { FontAwesome } from "@expo/vector-icons";
-import TabBarIcon from "../../components/TabBarIcon";
-import Colors from "../../constants/Colors";
+import TabBarIcon from "../../../components/TabBarIcon";
+import Colors from "../../../constants/Colors";
 
 const ShopSearchTabs = createBottomTabNavigator({
 
@@ -58,7 +58,7 @@ const ShopSearchTabs = createBottomTabNavigator({
 
 
 export default createStackNavigator(
-  { ShopScreenTabs }, 
+  { ShopSearchTabs }, 
   { 
     headerMode: "none" ,
   });
