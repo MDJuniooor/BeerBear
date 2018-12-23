b8b71120b7d156868e630426576e9243c7e55a4c
Zunwu <44821288+Zunwu@users.noreply.github.com>
2018-12-23 17:02:49 +0900

    Update index.js

--- frontend/screens/ShopSearchScreen/Map/index.js
+++ frontend/screens/ShopSearchScreen/Map/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View, FlatList } from 'react-native';
 import MapView from 'react-native-maps';
-import ShopInfo from  '../../../assets/BSInfo';
+import ShopInfo from  '../../../assets/BsInfo';
 
 
 export default class Map extends React.Component {
