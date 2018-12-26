5ecdbcf4b13175557e5d6bd5f38fcae6e5f2fa4a
stiria06 <32118818+stiria06@users.noreply.github.com>
2018-12-27 00:12:02 +0900

    Add files via upload

--- frontend/screens/BuddyMatchingScreen/BuddyInfo/index.js
+++ frontend/screens/BuddyMatchingScreen/BuddyInfo/index.js
@@ -1,64 +1,111 @@
-import React from "react";
+import React, { Component } from 'react';
 import {
-  AsyncStorage,
   StyleSheet,
-  View,
-  Button,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import {
-  Container,
-  Header,
-  Content,
-  Card,
-  CardItem,
   Text,
-  Body,
-  Thumbnail,
-  Left,
-  Icon
-} from "native-base";
-//import UserInfo from "..assets/UserInfo";
+  View,
+  TouchableHighlight,
+  Image,
+  Alert,
+  ScrollView,
+} from 'react-native';
 
+import users from '../../../assets/UserInfo2'
+export default class ProfileCardView extends Component {
 
-class Profile extends React.Component {
-  state = {
-    
-  };
-  onPressButton(){
-    //누르면 대화요청.
+  constructor(props) {
+    super(props);
   }
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "친구 요청이 완료되었습니다! "+viewId);
+  }
+
   render() {
-   // let name = UserInfo[0].name;
-   // let age = UserInfo[0].age;
-   // let sex = UserInfo[0].sex;
-   // let photo = UserInfo[0].photo;
-    //let introduce = UserInfo[0].introduce;
-
     return (
-      <Container>
-        <View style={{ }}>
-          <Image source={{uri:photo}} />
-          <Text> {name} </Text>
-          <Text> {sex}, {age} </Text>
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.container}>
+          <View style={styles.box}>
+            <Image style={styles.profileImage} source={{uri: 'https://pbs.twimg.com/profile_images/838333189957398529/OEdnN3LU_400x400.jpg'}}/>
+            <Text style={styles.name}>주누 준우</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableHighlight style={[styles.button, styles.buttonMessage]} onPress={() => this.onClickListener('')}>
+              <Text>친구요청하시겠습니까?</Text>
+            </TouchableHighlight>
+          </View>
         </View>
-        <View style={{ }}>
-          <Text>{introduce}</Text>
-        </View>
-
-      </Container>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scrollContainer:{
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff"
-  }
-});
+  },
+  container:{
+    padding:20,
+  },
+  box: {
+    marginTop:10,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOpacity: .2,
+    shadowOffset: {
+      height:1,
+      width:-2
+    },
+    elevation:2,
+    paddingTop:10
+  },
+  profileImage:{
+    width:300,
+    height:300,
+    marginBottom:20,
+  },
+  name:{
+    fontSize:35,
+    marginBottom:20,
+    fontWeight: 'bold',
+    color: '#1E90FF',
+  },
+  buttonContainer:{
+    flexDirection:'row',
+    marginTop:20,
+  },
 
-export default Profile;
+  button: {
+    width:'100%',
+    height:60,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    borderRadius:30,
+    margin:10,
+    shadowColor: 'black',
+    shadowOpacity: .8,
+    shadowOffset: {
+      height:2,
+      width:-2
+    },
+    elevation:4,
+  },
+  buttonMessage: {
+    backgroundColor: "#00BFFF",
+  },
+  buttonLike: {
+    backgroundColor: "#228B22",
+  },
+  buttonLove: {
+    backgroundColor: "#FF1493",
+  },
+  buttonCall: {
+    backgroundColor: "#40E0D0",
+  },
+  icon: {
+    width:35,
+    height:35,
+  }
+}); 
